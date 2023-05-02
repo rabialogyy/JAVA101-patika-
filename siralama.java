2dfcc3fe71b0780f26d587a37f6d3b3eb5ce58cd
rabialogy <rabiarencber@aof.anadolu.edu.tr>
2023-05-02 15:42:29 +0300

    sayı sıralama
    sayı sıralama

---
+++ siralama.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class siralama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("1. sayıyı Giriniz");
+        a = input.nextInt();
+
+        System.out.print("2. sayıyı Giriniz");
+        b = input.nextInt();
+
+        System.out.print("3. sayıyı Giriniz");
+        c = input.nextInt();
+
+        if (a < b && a < c) {
+            if (b < c) {
+
+                System.out.println("a < b < c");
+            } else {
+                System.out.println("a < c < b");
+            }
+        } else if (b < a && b < c) {
+            if (a < c) {
+                System.out.println("b<a<c");
+            } else{
+                System.out.println("b<c<a");
+            }
+        } else if (c<a && c<b) {
+            if (a<b){
+                System.out.println("c<a<b");}
+            else {
+                System.out.println("c<b<a");
+
+        }
+
+    }
+
+
+}
+
+
+
+}

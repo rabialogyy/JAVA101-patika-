6e003f5ee5e9d45409bc3cc0e1652a3255c9dcb3
rabialogy <129234167+rabialogyy@users.noreply.github.com>
2023-05-06 21:43:21 +0300

    Update harmonik.java

--- harmonik.java
+++ harmonik.java
@@ -6,13 +6,13 @@ public class harmonik {
         System.out.println("Harmonik Seri");
 
 
-        System.out.println("N saysını giriniz");
+        System.out.println("N sayısını giriniz:");
         int n =input.nextInt();
         double result=0.0;
 
 
         for ( int i =1;i<=n; i++){
-            result+=(1.0 /i); // double / int = double değer( double değer için birinin double olması yeterli.)
+            result+=(1.0 /i); // double / int = double değer ( double değer için birinin double olması yeterli.)
                               // int / int= int
 
 

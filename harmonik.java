94ceb43d202e743a987cf58e0292a5baa07ff85c
rabialogy <rabiarencber@aof.anadolu.edu.tr>
2023-05-06 21:41:20 +0300

    harmonik  sayı hesaplama
    harmonik sayı hesaplama

---
+++ harmonik.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class harmonik {
+    public static void main (String []args){
+
+        Scanner input = new Scanner ( System.in);
+        System.out.println("Harmonik Seri");
+
+
+        System.out.println("N saysını giriniz");
+        int n =input.nextInt();
+        double result=0.0;
+
+
+        for ( int i =1;i<=n; i++){
+            result+=(1.0 /i); // double / int = double değer( double değer için birinin double olması yeterli.)
+                              // int / int= int
+
+
+        }
+        System.out.println(result);
+
+
+
+
+
+
+
+    }
+
+
+}
